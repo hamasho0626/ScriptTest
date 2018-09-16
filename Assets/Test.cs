6c2b2da8e17697e20e54354dd28bb3a122c21671
hamasho0626 <kurono235@gmail.com>
2018-09-16 15:23:49 +0900

    test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -11,6 +11,7 @@ public class Test : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
+
         int[] points = { 20, 30, 40, 50, 60 };
 
         for (int i = 0; i < points.Length; i++)
